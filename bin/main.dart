e01e50d307c333d5ec9be392adad4ae96e5c32a7
unknown <gwang907@gmail.com>
2025-03-20 12:31:39 +0900

    feat: 보너스 체력 제공 기능 추가
    Game class 내 randomAdditionalHp 메서드

--- bin/main.dart
+++ bin/main.dart
@@ -4,6 +4,15 @@ void main() async {
   Game game = Game();
   await game.makePlayerChar();
   await game.makeMonList();
-  game.player.getName();
-  game.startGame();
+
+  ///if 문으로 게임 시작 조건(characdters, monsters파일이 정상적으로 읽혔는지) 확인
+  ///true일 때만 게임 시작, false인 경우 멘트 출력 후 게임 종료
+  if (game.charIsValid && game.monIsValid) {
+    game.player.getName();
+    game.randomAdditionalHp();
+    game.startGame();
+  } else {
+    print('게임 진행에 필요한 파일을 불러오지 못했습니다.');
+    print('게임을 종료합니다.');
+  }
 }
--- lib/game.dart
+++ lib/game.dart
@@ -5,9 +5,15 @@ import 'package:pp_rpg_game/character.dart';
 import 'package:pp_rpg_game/monster.dart';
 
 class Game {
-  ///character 클래스 넣기
   List<Monster> monsters = [];
   int victory = 0;
+
+  Character player = Character('name', 1, 1, 1);
+  List<Monster> monList = [];
+
+  ///게임 시작 조건 관리
+  bool charIsValid = false;
+  bool monIsValid = false;
 
   //게임의 상태 관리
   bool isStart = true;
@@ -19,10 +25,72 @@ class Game {
   ///게임 결과 관리
   String result = '';
 
-  Character player = Character('name', 1, 1, 1);
-  List<Monster> monList = [];
-
   Monster currentMon = Monster('', 1, 1);
+
+  ///class 내부 변수 선언 끝
+  ///
+  ///
+  ///
+  ///필수기능 메서드 시작
+
+  ///파일에서 플레이어 정보 가져오기
+  Future<void> makePlayerChar() async {
+    try {
+      ///캐릭터 파일 불러오기
+      var charFile = File('assets/characters.csv');
+
+      ///파일 내부 정보를 리스트화
+      List<String> charInfo = (await charFile.readAsString()).split(',');
+      player = Character(
+        'name',
+        int.parse(charInfo[0]),
+        int.parse(charInfo[1]),
+        int.parse(charInfo[2]),
+      );
+
+      ///게임 시작 조건 -- characters 파일 읽어오기 true
+      charIsValid = true;
+
+      ///플레이어 기본정보 적용 완료
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  ///파일에서 몬스터 정보 가져와 리스트화
+  Future<void> makeMonList() async {
+    try {
+      ///몬스터 파일 불러오기
+      var monFile = File('assets/monsters.csv');
+
+      ///파일 내부 정보 리스트화
+      Stream<String> lines = monFile
+          .openRead()
+          .transform(utf8.decoder)
+          .transform(LineSplitter());
+      await for (var line in lines) {
+        List<String> monInfo = line.split(',');
+        Monster monster = Monster(
+          monInfo[0],
+          int.parse(monInfo[1]),
+          int.parse(monInfo[2]),
+        );
+
+        ///몬스터 maxAtk 이용해서 랜덤 atk 설정하기
+        ///
+        Random random = Random();
+        int atk = random.nextInt(monster.maxAtk - player.def + 1) + player.def;
+        monster.atk = atk;
+
+        monList.add(monster);
+      }
+
+      ///게임 시작 조건 -- monsters 파일 읽어오기 true
+      monIsValid = true;
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
 
   Future<void> startGame() async {
     while (true) {
@@ -113,64 +181,7 @@ class Game {
     }
   }
 
-  ///파일에서 플레이어 정보 가져오기
-  Future<void> makePlayerChar() async {
-    try {
-      ///캐릭터 파일 불러오기
-      var charFile = File('assets/characters.csv');
-
-      ///파일 내부 정보를 리스트화
-      List<String> charInfo = (await charFile.readAsString()).split(',');
-      player = Character(
-        'name',
-        int.parse(charInfo[0]),
-        int.parse(charInfo[1]),
-        int.parse(charInfo[2]),
-      );
-
-      ///플레이어 기본정보 적용 완료
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
-
-  getPlayerChar() {
-    return player;
-  }
-
-  ///파일에서 몬스터 정보 가져와 리스트화
-  Future<void> makeMonList() async {
-    try {
-      ///몬스터 파일 불러오기
-      var monFile = File('assets/monsters.csv');
-
-      ///파일 내부 정보 리스트화
-      Stream<String> lines = monFile
-          .openRead()
-          .transform(utf8.decoder)
-          .transform(LineSplitter());
-      await for (var line in lines) {
-        List<String> monInfo = line.split(',');
-        Monster monster = Monster(
-          monInfo[0],
-          int.parse(monInfo[1]),
-          int.parse(monInfo[2]),
-        );
-
-        ///몬스터 maxAtk 이용해서 랜덤 atk 설정하기
-        ///
-        Random random = Random();
-        int atk = random.nextInt(monster.maxAtk - player.def + 1) + player.def;
-        monster.atk = atk;
-
-        monList.add(monster);
-      }
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
-
-  getRandomMon() {
+  void getRandomMon() {
     print('새로운 몬스터가 나타났습니다!');
     int currentMonIndex = Random().nextInt(monList.length);
     currentMon = monList[currentMonIndex];
@@ -218,5 +229,20 @@ class Game {
       encoding: Encoding.getByName('utf-8')!,
     );
     print('결과가 저장되었습니다.');
+  }
+
+  ///필수기능 메서드 끝
+  ///
+  ///
+  ///
+  ///도전기능 메서드 시작
+
+  void randomAdditionalHp() {
+    Random random = Random();
+    int chance = random.nextInt(10);
+    if (chance <= 2) {
+      player.hp += 10;
+      print('보너스 체력을 얻었습니다! 현재 체력: ${player.hp}');
+    }
   }
 }
